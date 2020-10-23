b835205a7de24a6b2a1a84733733ffcda7cf4c48
hrek_GZJ <hrek@qq.com>
2020-10-23 15:23:25 +0800

    frame

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <top />
+    <index />
+  </div>
+</template>
+
+<script>
+import top from '@/components/top';
+import index from '@/components/index';
+export default {
+  name: 'app',
+  components: {
+    top,
+    index
+  }
+};
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="index-container">
+    <div class="nav" >
+      <ul>
+        <li>
+          <router-link to="/discovery">
+            <span class="iconfont icon-find-music"></span>
+            发现音乐
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/playlists">
+            <span class="iconfont icon-music-list"></span>
+            推荐歌单
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/songs">
+            <span class="iconfont icon-music"></span>
+            最新音乐
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/mvs">
+            <span class="iconfont icon-mv"></span>
+            最新MV
+          </router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="main">
+      <router-view></router-view>
+    </div>
+    <div class="player">
+      <audio controls autoplay :src="url" loop></audio>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data() {
+    return {
+      activeIndex: 0,
+      url:""
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+
+</style>
